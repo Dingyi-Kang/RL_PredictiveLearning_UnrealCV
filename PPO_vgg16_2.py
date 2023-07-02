83782d2bd588ab9a69cdcb0f49cb152f0eb71af3
Dingyi-Kang <dkang@okstate.edu>
2023-07-02 15:49:10 -0500

    clean some comments

--- PPO_vgg16_2.py
+++ PPO_vgg16_2.py
@@ -125,9 +125,6 @@ def create_cnn(observation_dimensions, num_actions):
     
     for layer in vgg16_4blocks.layers:
         print("Layer name:", layer.name)
-        print("Layer configuration:")
-        print(layer.get_config())
-        print("\n")
 
     # We use the vgg16_4blocks model in a TimeDistributed manner
     x = keras.layers.TimeDistributed(vgg16_4blocks)(input_tensor)
